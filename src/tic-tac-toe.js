21559108ba18b1b43460ef2e4aeea3808e4fec3e
Zakhar Slabodnik <zakhar.slabodnik@gmail.com>
2017-02-05 23:23:37 +0300

    Implemented specs

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,70 @@
+
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.ticTacBoard = [[null, null, null],[null, null, null],[null, null, null]];
+    this.currentPlayerSymbol = 'x';
+    this.step = 0;
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayerSymbol;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.ticTacBoard[rowIndex][columnIndex] == null) {
+      this.ticTacBoard[rowIndex][columnIndex] = this.currentPlayerSymbol;
+
+      if (this.currentPlayerSymbol == 'x') {
+        this.currentPlayerSymbol = 'o';
+      } else {
+        this.currentPlayerSymbol = 'x';
+      }
+      this.step += 1;
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return this.noMoreTurns() || this.getWinner() != null;
+  }
 
+  getWinner() {
+    let winner =
+      (this.ticTacBoard[0][0] == this.ticTacBoard[0][1] && this.ticTacBoard[0][1] == this.ticTacBoard[0][2] && this.ticTacBoard[0][2] == this.ticTacBoard[0][0] && this.ticTacBoard[0][0]) ||
+      (this.ticTacBoard[1][1] == this.ticTacBoard[1][0] && this.ticTacBoard[1][0] == this.ticTacBoard[1][2] && this.ticTacBoard[1][2] == this.ticTacBoard[1][1] && this.ticTacBoard[1][1]) ||
+      (this.ticTacBoard[2][0] == this.ticTacBoard[2][1] && this.ticTacBoard[2][1] == this.ticTacBoard[2][2] && this.ticTacBoard[2][2] == this.ticTacBoard[2][0] && this.ticTacBoard[2][0]) ||
+
+      (this.ticTacBoard[0][0] == this.ticTacBoard[1][0] && this.ticTacBoard[1][0] == this.ticTacBoard[2][0] && this.ticTacBoard[2][0] == this.ticTacBoard[0][0] && this.ticTacBoard[0][0]) ||
+      (this.ticTacBoard[1][1] == this.ticTacBoard[0][1] && this.ticTacBoard[0][1] == this.ticTacBoard[2][1] && this.ticTacBoard[2][1] == this.ticTacBoard[1][1] && this.ticTacBoard[1][1]) ||
+      (this.ticTacBoard[0][2] == this.ticTacBoard[1][2] && this.ticTacBoard[1][2] == this.ticTacBoard[2][2] && this.ticTacBoard[2][2] == this.ticTacBoard[0][2] && this.ticTacBoard[0][2]) ||
+
+      (this.ticTacBoard[1][1] == this.ticTacBoard[0][2] && this.ticTacBoard[0][2] == this.ticTacBoard[2][0] && this.ticTacBoard[2][0] == this.ticTacBoard[0][2] && this.ticTacBoard[1][1]) ||
+      (this.ticTacBoard[0][0] == this.ticTacBoard[1][1] && this.ticTacBoard[1][1] == this.ticTacBoard[2][2] && this.ticTacBoard[2][2] == this.ticTacBoard[0][0] && this.ticTacBoard[0][0])
+
+    if (!winner) {
+      return null;
     }
+    return winner;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
-
+  noMoreTurns() {
+    if (this.step == 9) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    isFinished() {
-
+  isDraw() {
+    if (this.noMoreTurns() && this.getWinner() == null) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.ticTacBoard[rowIndex][colIndex]
+  }
 }
 
 module.exports = TicTacToe;
